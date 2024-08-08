609cb49d2a997184526825455129460f3532d5c3
Tim <0xtimc@gmail.com>
2024-08-08 14:36:06 +0200

    Add crash endpoint for a stacktrace

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -8,4 +8,10 @@ func routes(_ app: Application) throws {
     app.get("hello") { req async -> String in
         "Hello, world!"
     }
+
+    app.get("crash") { req -> HTTPStatus in
+        let array = [Int]()
+        let number = array[0]
+        return .ok
+    }
 }
